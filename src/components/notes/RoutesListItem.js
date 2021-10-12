a5464e6fe3e56153b38fa3d42708d2cbcd09dd1d
RischDev <risch1996@gmail.com>
2021-10-12 10:51:49 -0700

    Updated RoutesListItem to fetch notes asynchronously.

--- src/components/notes/RoutesListItem.js
+++ src/components/notes/RoutesListItem.js
@@ -1,52 +1,56 @@
-import React from 'react';
+import React, { Suspense } from 'react';
+import { Link } from "react-router-dom";
 import styles from './styles/RoutesListItem.Module.css';
 import Icon from '../common/Icon';
+import useSuspenseResource from '../common/useSuspense';
 
-import { Link } from "react-router-dom";
-
-class RoutesListItem extends React.Component {
-    constructor(props) {
-        super(props);
-
-        let path = props.path
-        let route = require('../../notes/' + path + '.json');
-
-        this.state = {
-            gameId: props.gameId,
-            path: path,
-            route: route
-        }
-    }
-
-    render() {
-        return(
-            <div className={styles.wrapper}>
-                <Link to={"/notes/game/" + this.state.gameId + "/route/" + this.state.path} className={`${styles.link} col-5 col-m-6`}>
-                    <div>
-                        {this.state.route.title}
-                    </div>
-                </Link>
-                <Link to={"/notes/game/" + this.state.gameId + "/route/" + this.state.path} className={`${styles.link} col-2 hidden-mobile`}>
-                    <div>
-                        {this.state.route.category}
-                    </div>
-                </Link>
-                <Link to={"/notes/game/" + this.state.gameId + "/route/" + this.state.path} className={`${styles.link} col-2 col-m-4`}>
-                    <div>
-                        {this.state.route.author}
-                    </div>
-                </Link>
-                <Link to={"/notes/game/" + this.state.gameId + "/route/" + this.state.path} className={`${styles.link} col-2 hidden-mobile`}>
-                    <div>
-                        {this.state.route.version}
-                    </div>
-                </Link>
-                <Link to={"/notes/editRoute/" + this.state.path} className={`${styles.link} col-1 col-m-2`}>
-                    <Icon src="/icons/edit.png" size="small" altText="Edit" hover={true} hidden={false} />
-                </Link>
-            </div>
+function RoutesListItem(props) {
+    const path = props.path;
+    const notesResource = useSuspenseResource(async () => {
+        const response = await fetch(
+            `${process.env.PUBLIC_URL}/notes/${path}.json`,
         );
-    }
+
+        return await response.json();
+    }, [path]);
+
+    return (
+        <Suspense fallback="Loading...">
+            <RoutesListItemImpl {...props} notesResource={notesResource} gameId={props.gameId} path={props.path} />
+        </Suspense>
+    );
+}
+
+function RoutesListItemImpl(props) {
+    const notes = props.notesResource.read();
+
+    return(
+        <div className={styles.wrapper}>
+            <Link to={"/notes/game/" + props.gameId + "/route/" + props.path} className={`${styles.link} col-5 col-m-6`}>
+                <div>
+                    {notes.title}
+                </div>
+            </Link>
+            <Link to={"/notes/game/" + props.gameId + "/route/" + props.path} className={`${styles.link} col-2 hidden-mobile`}>
+                <div>
+                    {notes.category}
+                </div>
+            </Link>
+            <Link to={"/notes/game/" + props.gameId + "/route/" + props.path} className={`${styles.link} col-2 col-m-4`}>
+                <div>
+                    {notes.author}
+                </div>
+            </Link>
+            <Link to={"/notes/game/" + props.gameId + "/route/" + props.path} className={`${styles.link} col-2 hidden-mobile`}>
+                <div>
+                    {notes.version}
+                </div>
+            </Link>
+            <Link to={"/notes/editRoute/" + props.path} className={`${styles.link} col-1 col-m-2`}>
+                <Icon src="/icons/edit.png" size="small" altText="Edit" hover={true} hidden={false} />
+            </Link>
+        </div>
+    );
 }
 
 export default RoutesListItem;
