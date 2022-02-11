1b7b77666886967c87cd4698064d225053855b41
RischDev <risch1996@gmail.com>
2022-02-11 14:42:01 -0800

    Fixed a bug where adding an image to a route would undo several previous changes to the route.

--- src/components/notes/routeForm/Section/SectionImage.js
+++ src/components/notes/routeForm/Section/SectionImage.js
@@ -1,35 +1,28 @@
-import React from 'react';
+import { useCallback } from 'react';
 import PreviewImage from './PreviewImage';
 import Icon from '../../../common/Icon';
 
-function shouldUpdate(oldProps, newProps) {
-    if (oldProps.image !== newProps.image) {
-        return false;
-    }
-
-    return true;
-}
-
-const SectionImage = React.memo((props) => {
-    const getBase64 = (file, callback) => {
+function SectionImage(props) {
+    const getBase64 = useCallback((file) => {
         let reader = new FileReader();
         reader.readAsDataURL(file);
         reader.onload = function () {
-            callback(reader.result)
+            props.updateImage(reader.result)
         };
         reader.onerror = function (error) {
             console.log('Error: ', error);
         };
-    }
+    },
+    [props]);
 
     return(
         <div>
             <label>Image: </label>
-            <input type="file" onChange={ (e) => { getBase64(e.target.files[0], props.updateImage) } } />
+            <input type="file" onChange={ (e) => { getBase64(e.target.files[0]) } } />
             <Icon src="/icons/delete.png" size="small" hover={true} hidden={props.image == null} altText="X" onClick={props.deleteImage} />
             <PreviewImage image={props.image} />
         </div>
     );
-}, shouldUpdate);
+}
 
 export default SectionImage;
